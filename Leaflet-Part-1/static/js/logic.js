c189c946a9d1e864049d3f15d8e06a0c06b2e2e7
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-10-10 22:22:43 -0500

    added legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -14,50 +14,103 @@ let response = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // get GeoJSON data
 d3.json(response).then (function(data){
-    L.geoJSON(data, {style: function(feature){
+    
+    // create geojson layer
+    let geojson = L.geoJSON(data, {style: function(feature){
         return{
-            color : "000000",
+            opacity: 1,
+            fillOpacity: 1,
             fillColor: depthColor(feature.geometry.coordinates[2]),
+            color: "#000000",
             radius: magRadius(feature.properties.mag),
-        }
+            stroke: true,
+            weight: 0.5,
+        },
+        L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]], 
+            {radius : 500}).addTo(map);
+    
     },
     
+        // bind a popup to each layer
         onEachFeature: function(feature, layer){
-        layer.bindPopup(`<h3>${feature.properties.place}</h3> <h4>Magnitude : ${feature.properties.mag}</h4>`)
-    }},
-    {pointToLayer:function(feature){
-        return L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
-    }}).addTo(map)
+            layer.bindPopup(`<h3>${feature.properties.place}</h3> <h4>Magnitude : ${feature.properties.mag}</h4>`)
+            }},
+            {pointToLayer:function(feature){
+                return L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
+            }}).addTo(map)
+    
+    // style function
+    function styleInfo(feature) {
+        return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: depthColor(feature.geometry.coordinates[2]),
+        color: "#000000",
+        radius: magRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+        };
+    }
+
+    // get color based on depth
+    // function depthColor (depth){
+    //     if (depth >= 1) return "#a3f600";
+    //     else if (depth >= 2) return "#dcf400";
+    //     else if (depth >= 3) return "#f7db11";
+    //     else if (depth >= 4) return "#fdb72a";
+    //     else if (depth >= 5) return "#fca663";
+    //     else return "#ff5f65";
+    // }
+
+    function depthColor(depth) {
+        return depth > 70 ? '#a3f600' :
+               depth > 50  ? '#dcf400' :
+               depth > 30  ? '#f7db11' :
+               depth > 10  ? '#fdb72a' :
+               depth > -10   ? '#fca663' :
+                               '#ff5f65';
+    }
+
+    // get size based on magnitude
+    function magRadius (mag){
+        if (mag == 0) return 1;
+        else return mag * 50
+    }
+
+    let legend = L.control({position: "bottomright"});
+    legend.onAdd = function(map){
+        let div = L.DomUtil.create("div", "info legend");
+        let grades = [-10,10,30,50,70,90];
+
+        for(i=0; i < grades.length; i++){
+
+
+            div.innerHTML +=
+            '<i style="background:' + depthColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+        // div.innerHTML +=  '<i style = "background: #dcf400"></i><span>-10-30</span><br></br>';
+        // div.innerHTML +=  '<i style = "background: #f7db11"></i><span>-30-50</span><br></br>';
+        // div.innerHTML +=  '<i style = "background: #fdb72a"></i><span>-50-70</span><br></br>';
+        // div.innerHTML +=  '<i style = "background: #fca663"></i><span>-70-90</span><br></br>';
+        // div.innerHTML +=  '<i style = "background: #ff5f65"></i><span>-90+</span><br></br>';
+        
+        return div;
+    };
+
+
+    // Add legend to map https://codepen.io/haakseth/pen/KQbjdO
+    legend.addTo(map);
+
+   
 });
 
 
-// style function
-function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: depthColor(feature.geometry.coordinates[2]),
-      color: "#000000",
-      radius: magRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
+
+    
 
 
 
-// get color based on depth
-function depthColor (depth){
-    if (depth >= 1) return "a3f600";
-    else if (depth >= 2) return "#dcf400";
-    else if (depth >= 3) return "#f7db11";
-    else if (depth >= 4) return "#fdb72a";
-    else if (depth >= 5) return "#fca663";
-    else return "#ff5f65";
-}
 
-// get size based on magnitude
-function magRadius (mag){
-    if (mag == 0) return 1;
-    else return mag * 50
-}
+
+
