647b76ecb915870085e0c72cca88a0adaa5540bf
UzmaSayyeda <uzmafatima5991@gmail.com>
2023-10-11 17:05:58 -0500

    fixed legend and debugged code

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // create map object
 let map = L.map('map', {
     center: [45.52, -122.67],
-    zoom: 4
+    zoom: 5
 });
 
 // create tile layer
@@ -9,108 +9,82 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
 {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
 .addTo(map);
 
-// use link to GeoJson data
+// use link to get GeoJson data
 let response = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// get GeoJSON data
-d3.json(response).then (function(data){
+
+
+// get GeoJson data
+d3.json(response).then(function(data){
     
-    // create geojson layer
-    let geojson = L.geoJSON(data, {style: function(feature){
-        return{
-            opacity: 1,
-            fillOpacity: 1,
-            fillColor: depthColor(feature.geometry.coordinates[2]),
-            color: "#000000",
-            radius: magRadius(feature.properties.mag),
-            stroke: true,
-            weight: 0.5,
-        },
-        L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]], 
-            {radius : 500}).addTo(map);
-    
-    },
-    
-        // bind a popup to each layer
-        onEachFeature: function(feature, layer){
-            layer.bindPopup(`<h3>${feature.properties.place}</h3> <h4>Magnitude : ${feature.properties.mag}</h4>`)
-            }},
-            {pointToLayer:function(feature){
-                return L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
-            }}).addTo(map)
-    
-    // style function
+    // style info function - 
     function styleInfo(feature) {
         return {
-        opacity: 1,
-        fillOpacity: 1,
-        fillColor: depthColor(feature.geometry.coordinates[2]),
-        color: "#000000",
-        radius: magRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: getColor(feature.geometry.coordinates[2]),
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
         };
     }
-
-    // get color based on depth
-    // function depthColor (depth){
-    //     if (depth >= 1) return "#a3f600";
-    //     else if (depth >= 2) return "#dcf400";
-    //     else if (depth >= 3) return "#f7db11";
-    //     else if (depth >= 4) return "#fdb72a";
-    //     else if (depth >= 5) return "#fca663";
-    //     else return "#ff5f65";
-    // }
-
-    function depthColor(depth) {
-        return depth > 70 ? '#a3f600' :
-               depth > 50  ? '#dcf400' :
-               depth > 30  ? '#f7db11' :
-               depth > 10  ? '#fdb72a' :
-               depth > -10   ? '#fca663' :
-                               '#ff5f65';
+    
+    // create function to get color based on depth
+    function getColor(depth) {
+        if (depth > 90) return '#EA2C2C';
+        else if (depth > 70) return '#EA822C';
+        else if (depth > 50) return '#EE9C00';
+        else if (depth > 30) return '#EECC00';
+        else if (depth > 10) return '#D4EE00';
+        else return  '#98EE00';
+         
     }
 
-    // get size based on magnitude
-    function magRadius (mag){
-        if (mag == 0) return 1;
-        else return mag * 50
+    // create function to get radius based on magnitude
+    function getRadius(magnitude){
+        if (magnitude === 0){
+            return 1;
+        } 
+        return (magnitude)*4;
     }
+    
+    // create geojson layer
+    L.geoJson(data, {
+        pointToLayer : function (feature, latlng){
+            return L.circleMarker(latlng)
+        },
+        style: styleInfo,
+       
+        // bind a popup to each layer
+        onEachFeature : function(feature, layer){
+            layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3> <h4>Magnitude : ${feature.properties.mag}</h4><h4>Depth : ${feature.geometry.coordinates[2]}</h4>`);
+        }
+    }).addTo(map);
+    
 
-    let legend = L.control({position: "bottomright"});
-    legend.onAdd = function(map){
-        let div = L.DomUtil.create("div", "info legend");
-        let grades = [-10,10,30,50,70,90];
 
-        for(i=0; i < grades.length; i++){
+    // set up legend
+    let legend = L.control({position: 'bottomright'});
 
+    legend.onAdd = function (map) {
 
+        let div = L.DomUtil.create('div', 'info legend');
+            let grades = [-10, 10, 30, 50, 70, 90];
+            // let colors = ["#98EE00", "#D4EE00","#EECC00","#EE9C00","#EA822C",
+            // "#EA2C2C"]
+            
+        // loop through our depth intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
             div.innerHTML +=
-            '<i style="background:' + depthColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-        // div.innerHTML +=  '<i style = "background: #dcf400"></i><span>-10-30</span><br></br>';
-        // div.innerHTML +=  '<i style = "background: #f7db11"></i><span>-30-50</span><br></br>';
-        // div.innerHTML +=  '<i style = "background: #fdb72a"></i><span>-50-70</span><br></br>';
-        // div.innerHTML +=  '<i style = "background: #fca663"></i><span>-70-90</span><br></br>';
-        // div.innerHTML +=  '<i style = "background: #ff5f65"></i><span>-90+</span><br></br>';
-        
+                "<i style='background:" + getColor(grades[i] + 1) + "'></i> " +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+
         return div;
     };
 
-
-    // Add legend to map https://codepen.io/haakseth/pen/KQbjdO
+    // add legend to the map
     legend.addTo(map);
 
-   
 });
-
-
-
-    
-
-
-
-
-
-
